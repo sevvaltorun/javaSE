92f1ad9edda0c475e6032820a863271ae8fcee19
sevvaltorun <sevvaltrn3@gmail.com>
2024-08-05 22:13:15 +0300

    new classes

---
+++ src/main/java/org/sevvaltorun/_3_Variables.java
@@ -0,0 +1,20 @@
+package org.sevvaltorun;
+
+public class _3_Variables {
+
+    public static void main(String[] args) {
+
+//        int number=2002;
+//        System.out.println(number);
+//
+//        int $number=22;
+//        System.out.println($number);
+//
+//        int _number=90;
+//        System.out.println(_number);
+
+
+
+
+    }
+}
