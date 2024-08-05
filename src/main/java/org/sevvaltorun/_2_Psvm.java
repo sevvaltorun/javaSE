44a0821b0eee59f3a311cdf44dea8f9b6eee8b82
sevvaltorun <sevvaltrn3@gmail.com>
2024-08-05 22:17:49 +0300

    add to psvm, operators and variables

--- src/main/java/org/sevvaltorun/_2_Psvm.java
+++ src/main/java/org/sevvaltorun/_2_Psvm.java
@@ -3,38 +3,38 @@ package org.sevvaltorun;
 public class _2_Psvm {
     public static void main(String[] args) {
         //operators
-//        int number= 27;
-//        System.out.println(number);
-        //number=number+1; or number+=1;
+        int number= 27;
+        System.out.println(number);
+       // number=number+1; or number+=1;
 
-//        number++;
-//        System.out.println(number);
-//        number--;
-//        System.out.println(number);
-//        number-=10;
-//        System.out.println(number);
-//
-//        int number2=20;
-//        System.out.println(number2); // 20
-//
-//        int aa = number2++;
-//        System.out.println(aa); //20
-//        System.out.println(number2); //21
-//
-//        int bb = ++number2;
-//        System.out.println(bb); //22
+        number++;
+        System.out.println(number);
+        number--;
+        System.out.println(number);
+        number-=10;
+        System.out.println(number);
+
+        int number2=20;
+        System.out.println(number2); // 20
+
+        int aa = number2++;
+        System.out.println(aa); //20
+        System.out.println(number2); //21
+
+        int bb = ++number2;
+        System.out.println(bb); //22
 
         int x =77;
         int y=22;
-//        System.out.println("Total:" + (x+y));
-//        System.out.println("Total:" + (x-y));
-//        System.out.println("Total:" + (x*y));
-//        System.out.println("Total:" + (x/y));
-//        System.out.println("Total:" + (x%y));
-//        boolean isResult=(x==y);
-//        System.out.println(isResult);
-//        boolean isNew = (x!=y);
-//        System.out.println(isNew);
+        System.out.println("Total:" + (x+y));
+        System.out.println("Total:" + (x-y));
+        System.out.println("Total:" + (x*y));
+        System.out.println("Total:" + (x/y));
+        System.out.println("Total:" + (x%y));
+        boolean isResult=(x==y);
+        System.out.println(isResult);
+        boolean isNew = (x!=y);
+        System.out.println(isNew);
 
 
     }
--- src/main/java/org/sevvaltorun/_3_Variables.java
+++ src/main/java/org/sevvaltorun/_3_Variables.java
@@ -4,14 +4,14 @@ public class _3_Variables {
 
     public static void main(String[] args) {
 
-//        int number=2002;
-//        System.out.println(number);
-//
-//        int $number=22;
-//        System.out.println($number);
-//
-//        int _number=90;
-//        System.out.println(_number);
+        int number=2002;
+        System.out.println(number);
+
+        int $number=22;
+        System.out.println($number);
+
+        int _number=90;
+        System.out.println(_number);
 
 
 
