46bdc298f014f4a41b46c83fa90a81577286e0b5
sevvaltorun <sevvaltrn3@gmail.com>
2024-08-07 14:58:54 +0300

    write data types

---
+++ src/main/java/org/sevvaltorun/_5_PrimitiveDataTypes.java
@@ -0,0 +1,25 @@
+package org.sevvaltorun;
+
+public class _5_PrimitiveDataTypes {
+    public static void main(String[] args) {
+        /*
+        byte myByte=10;
+        short myShort=1000;
+        int myInt=10000;
+        long myLong=100000000L;
+            -> sonuna küçük/büyük L eklenmeli.
+ */
+        float myFloat=3.14f;
+        double myDouble=3.14159;
+
+        boolean myBoolean= true;
+        char myChar='A';
+
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+    }
+
+
+}
---
+++ src/main/java/org/sevvaltorun/_6_WrapperDataTypes.java
@@ -0,0 +1,30 @@
+package org.sevvaltorun;
+
+public class _6_WrapperDataTypes  {
+    public static void main(String[] args) {
+        /*
+        Byte: byte türünü sarmalar
+        Short: short türünü sarmalar
+        Integer: int türünü sarmalar
+        Long: long türünü sarmalar
+
+        Float: float türünü sarmalar
+        Double: double türünü sarmalar
+
+        Boolean: boolean türünü sarmalar
+        Character: char türünü sarmalar
+         */
+
+        Integer myInt = new Integer(10);
+        Double myDouble= new Double(3.14);
+        Boolean myBoolean = new Boolean(true);
+        Character myChar= new Character('B');
+
+        System.out.println(myInt);
+        System.out.println(myDouble);
+        System.out.println(myBoolean);
+        System.out.println(myChar);
+
+
+    }
+}
