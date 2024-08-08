4523afed6c307c7f6d19e35b9393aa3df9161e5b
sevvaltorun <sevvaltrn3@gmail.com>
2024-08-08 14:18:11 +0300

    add new folders

--- src/main/java/org/sevvaltorun/_4_DataTypes.java
+++
@@ -1,7 +0,0 @@
-package org.sevvaltorun;
-
-public class _4_DataTypes {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/main/java/org/sevvaltorun/_4_DataTypesAll.java
@@ -0,0 +1,55 @@
+package org.sevvaltorun;
+
+public class _4_DataTypesAll {
+    public static void main(String[] args) {
+        //PRIMITIVE
+        //Tam sayılar
+        //8 bit = 1 byte
+        byte   b1= 127; // -128 ile 127 arasında (8 bit)
+        short s1=32767; //-32.768 ile 32.767 arasında (16 bit)
+        int i1=2147483647; //-2.147.483.648 ile 2.147.483.647 arasında (32 bit)
+        long l1=9223372036857L; //-9.223.372.036.858 ile -9.223.372.036.857 arasında (64 bit)
+
+        //Virgüllü Sayılar
+        float f1= 14.53f; //(32 bit)
+        double  d1= 14.53; //
+
+        //boolean
+        boolean b3= false;
+        boolean b3Data= 3>1;
+
+        //char
+        char  c5='T';
+
+        System.out.println(c5);
+        System.out.println(i1);
+        System.out.println(b3);
+        System.out.println(b3Data);
+
+        // int data = null; YAPAMAYIZ :)
+
+        //WRAPPER
+        //Tam sayılar
+        //8 bit = 1 byte
+        Byte b11= 127;
+      //  Byte b12= new Byte(1);
+        Short s11=32767;
+        Integer i11= 2147483647;
+       // Integer i12= new Integer(41);
+        Long l11=32768L;
+
+        //Virgüllü Sayılar
+        Float f11= 14.53f;
+        Double d11 = 14.53;
+
+        //boolean
+        Boolean b13= false;
+        Boolean b23Data= 3>1;
+
+        //char
+        Character c11='S';
+
+        Integer data11= null; //WRAPPER DA NULL OLUR
+
+    }
+ }
