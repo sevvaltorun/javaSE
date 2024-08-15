0ff55f0cfafc4fecd269fa3f2b5abcdb15789230
sevvaltorun <sevvaltrn3@gmail.com>
2024-08-15 17:59:52 +0300

    add to for, while and do-while loops

---
+++ src/main/java/org/sevvaltorun/_16_loops.java
@@ -0,0 +1,22 @@
+package org.sevvaltorun;
+
+public class _16_loops {
+    public static void main(String[] args) {
+//        for(int i=12; i<= 20; i++){
+//            System.out.println(i);
+//        }
+
+//        int number=10;
+//        while (number<=50){
+//            System.out.println(number);
+//            number+=10;
+//        }
+
+        int number2=10;
+        do{
+            System.out.println(number2);
+            number2++;
+        }
+        while (number2<=15);
+    }
+}
